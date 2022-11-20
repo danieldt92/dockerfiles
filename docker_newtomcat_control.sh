1aff94f22087e496c0a9a4d5861a6009738b3c21
Daniel Dorado <danieldoradotalaveron@gmail.com>
2022-11-20 16:15:08 +0100

    Added Docker Image SH Control

---
+++ docker_newtomcat_control.sh
@@ -0,0 +1,23 @@
+docker stop $(docker ps -aq)
+if [ $? -eq 0 ]
+then
+        docker rm $(docker ps -aq)
+        if [ $? -eq 0 ]
+        then
+                docker rmi $(docker images -q)
+                if [ $? -eq 0 ]
+                then
+                        docker build -t tomcat .
+                        sleep 5
+                        docker run -d --name tomcat-ct -p 8081:8080 tomcat
+                else
+                        echo "No docker images to build"
+                fi
+        else
+                echo "No containers to remove"
+        fi
+else
+docker build -t tomcat .
+sleep 5
+docker run -d --name tomcat-ct -p 8081:8080 tomcat
+fi
